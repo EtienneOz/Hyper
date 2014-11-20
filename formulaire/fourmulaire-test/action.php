1150b6511a6cd1b99d07b1bc8e64733a608865d9
gsarah <sarahgarcin@hotmail.com>
2014-11-20 11:34:14 +0100

    write in a new file with name

--- formulaire/fourmulaire-test/action.php
+++ formulaire/fourmulaire-test/action.php
@@ -1,7 +1,9 @@
 <?php
-if(isset($_POST['field1']) && isset($_POST['field2'])) {
+if(isset($_POST['field1']) && isset($_POST['field2'])) { 
+	$username = $_POST['field1'];
     $data = $_POST['field1'] . '-' . $_POST['field2'] . "\n";
-    $ret = file_put_contents('data.txt', $data, FILE_APPEND | LOCK_EX);
+    $file = fopen("$username.txt", 'w');
+    $ret = file_put_contents("$username.txt", $data, FILE_APPEND | LOCK_EX);
     if($ret === false) {
         die('There was an error writing this file');
     }
